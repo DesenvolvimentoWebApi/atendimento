e4ee4dba8495e794faaf98fcb31043e02314e924
Moises Teodoro <moises_teodoro@hotmail.com>
2020-04-27 22:08:33 -0300

    Jpa Anotado

--- src/main/java/com/moisesteodoro/atendimento/model/Atendimento.java
+++ src/main/java/com/moisesteodoro/atendimento/model/Atendimento.java
@@ -2,11 +2,23 @@ package com.moisesteodoro.atendimento.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Atendimento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String sumario;
     private LocalDate criacao;
     private String descricao;
--- src/main/java/com/moisesteodoro/atendimento/model/Operador.java
+++ src/main/java/com/moisesteodoro/atendimento/model/Operador.java
@@ -1,10 +1,20 @@
 package com.moisesteodoro.atendimento.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Operador {
-
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String nome;
     private String email;
 
