fe8d51cce277a08aa5473da8ad68baa216a950d7
Moises Teodoro <moises_teodoro@hotmail.com>
2020-05-02 22:32:10 -0300

    Documentação de Classes

--- src/main/java/com/moisesteodoro/atendimento/model/Atendimento.java
+++ src/main/java/com/moisesteodoro/atendimento/model/Atendimento.java
@@ -13,23 +13,57 @@ import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/** 
+ * Um atendimento é uma interação iniciada pelo cliente e atendida por um operador.
+ */
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Atendimento {
 
+    /**
+     * Identificação do objeto
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    /**
+     * Breve descrição para quem o operador possar iniciar o atendimento
+     * @see #descricao
+     */
+
     private String sumario;
+
+    /**
+     * Data e hora da criação do atendiemento
+     */
     private LocalDate criacao;
+    
+    /**
+     * Descrição detalhada do atendimento
+     * @see #sumario
+     */
+
     private String descricao;
+
+    /**
+     * Tipo de atendimento de acordo com as opções disponíveis em {@link com.moisesteodoro.atendimento.model.TipoAtendimento Tipo de Atendimento}
+     */
     @Enumerated(EnumType.STRING)
     private TipoAtendimento tipo;
+
+    /**
+     * Cliente que requisitou o atendimento 
+     */
     @ManyToOne
     private Cliente cliente;
+
+    /**
+     * Operador responsável por dar prosseguimento ao atendimento
+     */
     @ManyToOne
     private Operador responsavel;
-    
+
 }
